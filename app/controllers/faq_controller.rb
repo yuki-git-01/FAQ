45eddf8c9962c9fa5df3118244491cbd3ad86f3a
EC2 Default User <ec2-user@ip-172-31-41-249.ap-northeast-1.compute.internal>
2020-03-01 10:06:43 +0000

    検索機能 リンク機能追加

--- app/controllers/faq_controller.rb
+++ app/controllers/faq_controller.rb
@@ -1,6 +1,15 @@
 class FaqController < ApplicationController
   
   def index
-    @faqs = Faq.all.limit(10)
+    @faqs = Faq.order('id desc').limit(10)
   end
+  
+  def search
+    @faqs = Faq.where('question LIKE(?)', "%#{params[:keyword]}%").limit(20)
+  end
+  
+  def show
+    @faqs = Faq.where('classification LIKE(?)', "%#{params[:classification]}%").limit(20)
+  end
+  
 end
